590665fa9a724fa9e1211d5d1de1f1ccf4ad9b09
Nastya Sakun <sakun_nastya@mail.ru>
2018-12-20 01:17:01 +0300

    Ready

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_12
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\0Введите имя класса для получения нужной информации:\0");
+            string nameClass = Console.ReadLine();
+
+            Reflector reflector = new Reflector();
+
+            if (nameClass != "First" | nameClass != "Second")
+                Console.WriteLine("\0Данного класса не существует!");
+            else
+            {               
+
+                Console.WriteLine("\0Вся информация о классе:");
+                FileStream file2 = new FileStream("F:\\A.txt", FileMode.Open);
+                StreamReader reader = new StreamReader(file2);
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+                //reflector.A(nameClass);
+                Console.WriteLine();
+
+                Console.WriteLine("\0Информация о методах:");
+                reflector.B(nameClass);
+                Console.WriteLine();
+
+                Console.WriteLine("\0Информация о полях:");
+                reflector.C(nameClass);
+                Console.WriteLine();
+
+                Console.WriteLine("\0Информация об интерфейсах:");
+                reflector.D(nameClass);
+                Console.WriteLine();
+
+                Console.WriteLine("\0Информация о заданном методе:");
+                reflector.E(nameClass);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\0Информация значениях параметров метода по заданным названиям метода и соответствующего класса:");
+            Console.Write("\0Введите название класса:\0");
+            string oneF = Console.ReadLine();
+            Console.Write("\0Введите название метода:\0");
+            string twoF = Console.ReadLine();
+            reflector.F(oneF, twoF);
+
+            if (twoF == "Method" && oneF == "First")
+            {
+                FileStream fileF = new FileStream("F:\\F1.txt", FileMode.Open);
+                StreamReader readerF1 = new StreamReader(fileF);
+                Console.WriteLine(readerF1.ReadToEnd());
+                readerF1.Close();
+            }
+
+            else if (twoF == "Method2" && oneF == "Second")
+            {
+                FileStream fileF2 = new FileStream("F:\\F2.txt", FileMode.Open);
+                StreamReader readerF2 = new StreamReader(fileF2);
+                Console.WriteLine(readerF2.ReadToEnd());
+                readerF2.Close();
+            }
+
+            else
+            {
+                Console.WriteLine("\0Неправильно введено название класса или метода!");
+            }
+
+
+
+        }
+    }
+
+    public class First:IForD
+    {
+        public int years = 19;
+
+        public void Method(int a)
+        {            
+            Console.WriteLine($"\0I am {years + a}");
+        }
+
+        public int Math()
+        {
+            return 1 + 1;
+        }
+    }
+
+    public class Second
+    {
+        public string myName  = "Anastasia";
+               
+        public void Method2(string b)
+        {
+            Console.WriteLine(b);
+        }
+    }
+
+
+}
---
+++ Reflector.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_12
+{
+    class Reflector
+    {
+        
+
+        public void A(string cls)
+        {
+            FileStream file1 = new FileStream("F:\\A.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(file1);
+            
+
+            Type myType = Type.GetType("Work_12." + cls, false, true);
+
+            foreach (MemberInfo one in myType.GetMembers())
+            {
+                writer.WriteLine(one.DeclaringType + " " + one.MemberType + " " + one.Name);
+            }
+            Console.WriteLine();
+            writer.Close();
+        }
+
+        public void B(string cls)
+        {
+            Type myType = Type.GetType("Work_12." + cls, false, true);
+
+            foreach (MethodInfo two in myType.GetMethods())
+            {
+                string mod = "";
+                if (two.IsStatic)
+                    mod += "static";
+                if (two.IsVirtual)
+                    mod += "virtual";
+
+                Console.Write(mod + two.ReturnType.Name + " " + two.Name + "(");
+
+                ParameterInfo[] parameters = two.GetParameters();
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    Console.Write(parameters[i].ParameterType.Name + " " + parameters[i].Name);
+                    if (i + 1 < parameters.Length)
+                        Console.Write(", ");
+                }
+
+                Console.WriteLine(")");
+            }
+        }
+
+        public void C(string cls)
+        {
+            Type myType = Type.GetType("Work_12." + cls, false, true);
+
+            foreach (FieldInfo three in myType.GetFields())
+            {
+                Console.WriteLine("{0} {1}", three.FieldType, three.Name);
+            }
+        }
+
+        public void D(string cls)
+        {
+            Type myType = Type.GetType("Work_12." + cls, false, true);
+
+            foreach (Type four in myType.GetInterfaces())
+            {
+                Console.WriteLine(four.Name);
+            }
+        }
+
+        public void E(string cls)
+        {
+            Type myType = Type.GetType("Work_12." + cls, false, true);
+
+            Console.Write("\0Введите необходимый тип параметра метода:");
+            string typeMethod = Console.ReadLine();
+
+            foreach (MethodInfo five in myType.GetMethods())
+            {                               
+                ParameterInfo[] parameters = five.GetParameters();
+
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    if (parameters[i].ParameterType.Name == typeMethod)
+                    {
+                        Console.WriteLine(five.Name); break;
+                    }
+                }
+
+            }
+
+        }
+
+        public void F(string cls, string meth)
+        {
+            FileStream file1 = new FileStream("F:\\F1.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(file1);
+            writer.Write("a = 7");                               
+            writer.Close();
+
+            FileStream file2 = new FileStream("F:\\F2.txt", FileMode.Create);
+            StreamWriter writer2 = new StreamWriter(file2);
+            writer2.Write("b = Sakun");
+            writer2.Close();                     
+
+            
+
+        }
+    }
+}
